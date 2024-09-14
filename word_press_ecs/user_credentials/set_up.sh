bc6fcf32f401f00fc2c56bc4c09f21329bf9503d
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 16:02:18 +1000

    Update set_up.sh

--- word_press_ecs/user_credentials/set_up.sh
+++ word_press_ecs/user_credentials/set_up.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 aws configure set aws_access_key_id $aws_access_key_id --profile cloud_user
 aws configure set aws_secret_access_key $aws_secret_access_key --profile cloud_user
 aws configure set region us-east-1 --profile cloud_user
