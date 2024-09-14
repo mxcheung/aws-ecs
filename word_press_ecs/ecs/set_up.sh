3ce74a92a5a328b964b467bcaee82b4cb22e4870
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 21:39:43 +1000

    Update set_up.sh

--- word_press_ecs/ecs/set_up.sh
+++ word_press_ecs/ecs/set_up.sh
@@ -61,6 +61,22 @@ ECS_TASK_DEFINITION=$(aws ecs register-task-definition \
 # Assign the VPC ID to a variable
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
 
+
+# Get Subnet ID for Database Subnet AZ A
+subnet_a=$(aws ec2 describe-subnets \
+    --filters "Name=tag:Name,Values=Database Subnet AZ A" "Name=availability-zone,Values=us-east-1a" \
+    --query "Subnets[0].SubnetId" --output text)
+
+# Get Subnet ID for Database Subnet AZ B
+subnet_b=$(aws ec2 describe-subnets \
+    --filters "Name=tag:Name,Values=Database Subnet AZ B" "Name=availability-zone,Values=us-east-1b" \
+    --query "Subnets[0].SubnetId" --output text)
+
+# Get Subnet ID for Database Subnet AZ C
+subnet_c=$(aws ec2 describe-subnets \
+    --filters "Name=tag:Name,Values=Database Subnet AZ C" "Name=availability-zone,Values=us-east-1c" \
+    --query "Subnets[0].SubnetId" --output text)
+    
 APP_SECURITY_GROUP_ID=$(aws ec2 create-security-group \
     --group-name app-sg \
     --description "Security group for application" \
