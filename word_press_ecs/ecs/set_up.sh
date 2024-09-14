5a56db83243ec5a74b0280f4229f4f60e6a02e69
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 19:38:32 +1000

    Update set_up.sh

--- word_press_ecs/ecs/set_up.sh
+++ word_press_ecs/ecs/set_up.sh
@@ -22,12 +22,29 @@ container_definitions=$(cat <<EOF
         "awslogs-region": "us-west-2",
         "awslogs-stream-prefix": "ecs"
       }
-    }
+    },
+    "environment": [
+      {
+        "name": "WORDPRESS_DB_HOST",
+        "valueFrom": "arn:aws:ssm:us-east-1:$AWS_ACCOUNT_ID:parameter/dev/WORDPRESS_DB_HOST"
+      },
+      {
+        "name": "WORDPRESS_DB_NAME",
+        "valueFrom": "arn:aws:ssm:us-east-1:$AWS_ACCOUNT_ID:parameter/dev/WORDPRESS_DB_NAME"
+      },
+      {
+        "name": "WORDPRESS_DB_USER",
+        "valueFrom": "arn:aws:secretsmanager:us-east-1:$AWS_ACCOUNT_ID:secret:rds!db-1380b131-f0b2-4ef3-833f-4ab7a78f29fd-BAsjMA:username::"
+      },
+      {
+        "name": "WORDPRESS_DB_PASSWORD",
+        "valueFrom": "arn:aws:secretsmanager:us-east-1:$AWS_ACCOUNT_ID:secret:rds!db-1380b131-f0b2-4ef3-833f-4ab7a78f29fd-BAsjMA:password::"
+      }
+    ]
   }
 ]
 EOF
 )
-
 
 ECS_TASK_DEFINITION=$(aws ecs register-task-definition \
     --family wordpress-td \
