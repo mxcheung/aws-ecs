ea1cee9cf12ee9d505cea4fd2f8c2cdfb404c96d
mxcheung <mxcheung@users.noreply.github.com>
2024-09-15 06:55:07 +1000

    Update set_up.sh

--- word_press_ecs/ecs/set_up.sh
+++ word_press_ecs/ecs/set_up.sh
@@ -8,10 +8,9 @@ image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/wordpress:latest"
 
 # Get the secret ARN
 SECRET_ARN=$(aws rds describe-db-instances --db-instance-identifier wordpress --query 'DBInstances[0].MasterUserSecret.SecretArn' --output text)
-
-
-echo "Secret Name: $SECRET_ARN"
-
+SECRET_NAME=$(aws secretsmanager describe-secret --secret-id $SECRET_ARN --query 'Name' --output text)
+echo "Secret ARN: $SECRET_ARN"
+echo "Secret Name: $SECRET_NAME"
 
 container_definitions=$(cat <<EOF
 [
@@ -44,11 +43,11 @@ container_definitions=$(cat <<EOF
       },
       {
         "name": "WORDPRESS_DB_USER",
-        "valueFrom": "$SECRET_ARN:username::"
+        "valueFrom": "$SECRET_NAME:username::"
       },
       {
         "name": "WORDPRESS_DB_PASSWORD",
-        "valueFrom": "$SECRET_ARN:password::"
+        "valueFrom": "$SECRET_NAME:password::"
       }
     ]
   }
