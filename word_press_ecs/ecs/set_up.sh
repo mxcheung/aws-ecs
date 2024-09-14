0fd6cf5834aa4f0a8d0dd95a0cee633d1f48f45a
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 19:32:25 +1000

    Create set_up.sh

---
+++ word_press_ecs/ecs/set_up.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/wordpress:latest"
+
+container_definitions=$(cat <<EOF
+[
+  {
+    "name": "wordpress",
+    "image": "$image_uri",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "protocol": "tcp"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "/ecs/wordpress",
+        "awslogs-region": "us-west-2",
+        "awslogs-stream-prefix": "ecs"
+      }
+    }
+  }
+]
+EOF
+)
+
+
+aws ecs register-task-definition \
+    --family wordpress-td \
+    --network-mode awsvpc \
+    --requires-compatibilities FARGATE \
+    --cpu "256" \
+    --memory "512" \
+    --execution-role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/OurEcsTaskExecutionRole \
+    --task-role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/OurEcsTaskRole \
+    --container-definitions "$container_definitions"
