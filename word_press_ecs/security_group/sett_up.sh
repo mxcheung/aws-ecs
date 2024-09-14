fa4c7a1c79fff2aca7481533ba6a7620f009c4c8
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 16:10:35 +1000

    Create sett_up.sh

---
+++ word_press_ecs/security_group/sett_up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assign the VPC ID to a variable
+VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
+
+
+
+SECURITY_GROUP_ID=$(aws ec2 create-security-group \
+    --group-name database-sg \
+    --description "Security group for RDS instance" \
+    --vpc-id "$VPC_ID" \
+    --query "GroupId" \
+    --output text)
+
+aws ec2 authorize-security-group-ingress \
+   --group-id $SECURITY_GROUP_ID \
+   --ip-permissions IpProtocol=tcp,FromPort=3306,ToPort=3306,IpRanges="[{CidrIp=10.0.0.0/16,Description='Allow MySQL access from VPC'}]"
+
+
