477eec6d0913977c8c2b24f8ba9993acb3169141
mxcheung <mxcheung@users.noreply.github.com>
2024-09-14 23:00:21 +1000

    Update set_up.sh

--- word_press_ecs/ecs/set_up.sh
+++ word_press_ecs/ecs/set_up.sh
@@ -4,6 +4,15 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/wordpress:latest"
 
+
+
+# Get the secret ARN
+SECRET_ARN=$(aws rds describe-db-instances --db-instance-identifier wordpress --query 'DBInstances[0].MasterUserSecret.SecretArn' --output text)
+
+
+echo "Secret Name: $SECRET_NAME"
+
+
 container_definitions=$(cat <<EOF
 [
   {
@@ -35,11 +44,11 @@ container_definitions=$(cat <<EOF
       },
       {
         "name": "WORDPRESS_DB_USER",
-        "valueFrom": "arn:aws:secretsmanager:us-east-1:$AWS_ACCOUNT_ID:secret:rds!db-1380b131-f0b2-4ef3-833f-4ab7a78f29fd-BAsjMA:username::"
+        "valueFrom": "$SECRET_ARN:username::"
       },
       {
         "name": "WORDPRESS_DB_PASSWORD",
-        "valueFrom": "arn:aws:secretsmanager:us-east-1:$AWS_ACCOUNT_ID:secret:rds!db-1380b131-f0b2-4ef3-833f-4ab7a78f29fd-BAsjMA:password::"
+        "valueFrom": "$SECRET_ARN:password::"
       }
     ]
   }
