8572f696173c96acbe29221d3e5442761174918e
mxcheung <mxcheung@users.noreply.github.com>
2024-09-16 22:53:35 +1000

    Update set_up.sh

--- word_press_ecs/ecs/set_up.sh
+++ word_press_ecs/ecs/set_up.sh
@@ -6,7 +6,7 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 # Assign the VPC ID to a variable
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
 
-$APP_SG_ID_2=$(aws ec2 describe-security-groups --filters Name=group-name,Values=app-sg --query "SecurityGroups[0].GroupId" --output text)
+APP_SG_ID_2=$(aws ec2 describe-security-groups --filters Name=group-name,Values=app-sg --query "SecurityGroups[0].GroupId" --output text)
 
 echo $APP_SG_ID_2
 
