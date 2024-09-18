d706be3d11b8d6c4e6829b26b06d3f33e4e48bcb
mxcheung <mxcheung@users.noreply.github.com>
2024-09-18 22:46:11 +1000

    Update set_up.sh

--- word_press_ecs/user_credentials/set_up.sh
+++ word_press_ecs/user_credentials/set_up.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+response=$(aws iam create-access-key --output json)
+
+# Extract AccessKeyId and SecretAccessKey from the response
+aws_access_key_id=$(echo $response | jq -r '.AccessKey.AccessKeyId')
+aws_secret_access_key=$(echo $response | jq -r '.AccessKey.SecretAccessKey')
+
+# Print the keys to verify
+echo "Access Key ID: $aws_access_key_id"
+echo "Secret Access Key: $aws_secret_access_key"
+
+
 aws configure set aws_access_key_id $aws_access_key_id --profile cloud_user
 aws configure set aws_secret_access_key $aws_secret_access_key --profile cloud_user
 aws configure set region us-east-1 --profile cloud_user
