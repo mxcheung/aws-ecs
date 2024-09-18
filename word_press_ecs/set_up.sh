884763e3489d7d84f8434a81838774ca31f2b7c8
mxcheung <mxcheung@users.noreply.github.com>
2024-09-18 23:09:21 +1000

    Update set_up.sh

--- word_press_ecs/set_up.sh
+++ word_press_ecs/set_up.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/user_credentials
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/user_credentials
 . ./set_up.sh
 aws logs create-log-group --log-group-name /ecs/wordpress-td
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/networks
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/networks
 . ./set_up.sh
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/rds
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/rds
 . ./set_up.sh  
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/ecs-cluster
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/ecs-cluster
 . ./set_up.sh
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/ecr
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/ecr
 . ./set_up.sh
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/ecs-task-definition
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/ecs-task-definition
 . ./set_up.sh
-cd /home/ec2-user/environment/aws-ecs/word_press_ecs/ecs
+cd /home/cloudshell-user/environment/aws-ecs/word_press_ecs/ecs
 . ./set_up.sh
