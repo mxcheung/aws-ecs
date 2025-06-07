161520043f2e91c1d49587edba7bf4841d3264e2
mxcheung <mxcheung@users.noreply.github.com>
2025-06-07 20:14:50 +1000

    Update set_up.sh

--- word_press_ecs/security_group/set_up.sh
+++ word_press_ecs/security_group/set_up.sh
@@ -4,15 +4,6 @@
 # Assign the VPC ID to a variable
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
 
-
-# Get the Security Group ID for ALBAllowHttp
-ALB_ALLOW_HTTP_SG_ID=$(aws ec2 describe-security-groups \
-    --filters Name=group-name,Values=ALBAllowHttp \
-    --query 'SecurityGroups[0].GroupId' \
-    --output text)
-
-echo $ALB_ALLOW_HTTP_SG_ID
-
 echo "Creating security Group  app-sg --> aws elbv2 create-security-group"
 
 APP_SG_ID_OUTPUT=$(aws ec2 create-security-group \
@@ -24,9 +15,15 @@ APP_SG_ID_OUTPUT=$(aws ec2 create-security-group \
 
 APP_SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=app-sg --query "SecurityGroups[0].GroupId" --output text)
 
-
 echo $APP_SG_ID
 
+# Get the Security Group ID for ALBAllowHttp
+ALB_ALLOW_HTTP_SG_ID=$(aws ec2 describe-security-groups \
+    --filters Name=group-name,Values=ALBAllowHttp \
+    --query 'SecurityGroups[0].GroupId' \
+    --output text)
+
+echo $ALB_ALLOW_HTTP_SG_ID
 
 echo "Add  app-sg inbound rule allow HTTP traffic from ALBAllowHttp --> aws ec2 authorize-security-group-ingress"
 
@@ -36,7 +33,6 @@ INGRESS_OUTPUT=$(aws ec2 authorize-security-group-ingress \
     --protocol tcp \
     --port 80 \
     --source-group $ALB_ALLOW_HTTP_SG_ID)
-
 
 echo "create-security-group database-sg"
 
