e41c6f7b7179fb6b01e3c5de0590d8c3e175061f
mxcheung <mxcheung@users.noreply.github.com>
2025-06-07 20:40:32 +1000

    Update set_up.sh

--- word_press_ecs/networks/set_up.sh
+++ word_press_ecs/networks/set_up.sh
@@ -3,19 +3,6 @@
 # Assign the VPC ID to a variable
 VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name' && Value=='Your Custom VPC']].{VpcId:VpcId}" --output text)
 
-APP_SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=app-sg --query "SecurityGroups[0].GroupId" --output text)
-
-echo $APP_SG_ID
-
-echo "Add  app-sg inbound rule allow HTTP traffic from ALBAllowHttp --> aws ec2 authorize-security-group-ingress"
-
-# Add an inbound rule to allow HTTP traffic from ALBAllowHttp
-INGRESS_OUTPUT=$(aws ec2 authorize-security-group-ingress \
-    --group-id $APP_SG_ID \
-    --protocol tcp \
-    --port 80 \
-    --source-group $ALB_ALLOW_HTTP_SG_ID)
-
 echo "Creating Target Group --> aws elbv2 create-target-group"
 
 CREATE_TARGET_GROUP_OUTPUT=$(aws elbv2 create-target-group \
