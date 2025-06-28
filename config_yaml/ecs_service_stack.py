a817c96bf537305af72e8c1669025cf38acbd885
mxcheung <mxcheung@users.noreply.github.com>
2025-06-28 16:30:50 +1000

    Create ecs_service_stack.py

---
+++ config_yaml/ecs_service_stack.py
@@ -0,0 +1,38 @@
+from aws_cdk import Stack
+from aws_cdk.aws_ec2 import InstanceType
+from aws_cdk.aws_ecs import (
+    Ec2Service,
+    Ec2TaskDefinition,
+    ContainerImage,
+    Cluster
+)
+from constructs import Construct
+
+
+class EcsServiceStack(Stack):
+    def __init__(self, scope: Construct, id: str, config: dict, cluster: Cluster, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        ecs_cfg = config["ecs"]
+        ecr_cfg = config["ecr"]
+
+        # Add EC2 capacity to existing cluster
+        cluster.add_capacity(
+            "DefaultAutoScalingGroup",
+            instance_type=InstanceType(ecs_cfg["instanceType"])
+        )
+
+        task_def = Ec2TaskDefinition(self, "TaskDef")
+        task_def.add_container(
+            "AppContainer",
+            image=ContainerImage.from_registry(f"{ecr_cfg['repositoryUri']}:{ecr_cfg['tag']}"),
+            memory_limit_mib=256
+        )
+
+        Ec2Service(
+            self,
+            "EcsService",
+            cluster=cluster,
+            task_definition=task_def,
+            service_name=ecs_cfg["serviceName"]
+        )
