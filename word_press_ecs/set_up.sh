061456b2167d678b3640db00564cb0426d42994c
mxcheung <mxcheung@users.noreply.github.com>
2025-07-06 23:43:28 +1000

    Update set_up.sh

--- word_press_ecs/set_up.sh
+++ word_press_ecs/set_up.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/user_credentials
+echo $MY_ENV_DIR
+
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/user_credentials
 . ./set_up.sh
 aws logs create-log-group --log-group-name /ecs/wordpress-td
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/security_group
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/security_group
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/networks
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/networks
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/rds
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/rds
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/s3
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/s3
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/ecs-cluster
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/ecs-cluster
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/ecr
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/ecr
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/ecs-task-definition
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/ecs-task-definition
 . ./set_up.sh
-cd /home/cloudshell-user/aws-ecs/word_press_ecs/ecs
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/ecs
 . ./set_up.sh
