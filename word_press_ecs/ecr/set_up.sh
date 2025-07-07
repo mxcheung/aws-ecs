32932eafb7ef7fc42c4133fe3782605160bd6864
mxcheung <mxcheung@users.noreply.github.com>
2025-07-08 09:38:42 +1000

    Update set_up.sh

--- word_press_ecs/ecr/set_up.sh
+++ word_press_ecs/ecr/set_up.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# Extract AccessKeyId and SecretAccessKey from environment variables
-aws_access_key_id=$AWS_ACCESS_KEY_ID
-aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
-
-# Print the extracted values (optional)
-echo "AccessKeyId: $aws_access_key_id"
-echo "SecretAccessKey: $aws_secret_access_key"
-
-aws configure set aws_access_key_id $aws_access_key_id --profile cloud_user
-aws configure set aws_secret_access_key $aws_secret_access_key --profile cloud_user
-aws configure set region us-east-1 --profile cloud_user
-aws configure set output json --profile cloud_user
 export AWS_PROFILE=cloud_user
-sleep 5
 AWS_CALLER_IDENTITY=$(aws sts get-caller-identity)
 echo $AWS_CALLER_IDENTITY
 
