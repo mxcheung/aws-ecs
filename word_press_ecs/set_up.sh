21bf77d109f0c643a20ac26606175515dfea8f37
mxcheung <mxcheung@users.noreply.github.com>
2025-07-08 12:36:20 +1000

    Update set_up.sh

--- word_press_ecs/set_up.sh
+++ word_press_ecs/set_up.sh
@@ -12,8 +12,8 @@ echo $AWS_ACCESS_KEY_ID
 # . ./set_up.sh
 
 
-#cd $MY_ENV_DIR/aws-ecs/word_press_ecs/user_credentials
-#. ./set_up_no_create_access_key.sh
+cd $MY_ENV_DIR/aws-ecs/word_press_ecs/user_credentials
+. ./set_up_no_create_access_key.sh
 
 
 aws logs create-log-group --log-group-name /ecs/wordpress-td
