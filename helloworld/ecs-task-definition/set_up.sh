82fddf93b9d35ac4a7c53966532bdc42b8b61f3a
mxcheung <mxcheung@users.noreply.github.com>
2025-07-10 23:00:49 +1000

    Create set_up.sh

---
+++ helloworld/ecs-task-definition/set_up.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+image_uri="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/wordpress:latest"
+
+
+container_definitions=$(cat <<EOF
+[
+  {
+    "name": "wordpress",
+    "image": "$image_uri",
+    "essential": true,
+    "portMappings": [
+      {
+        "name": "wordpress-80-tcp",
+        "containerPort": 80,
+        "hostPort": 80,
+        "protocol": "tcp",
+        "appProtocol": "http"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "/ecs/wordpress-td",
+        "awslogs-create-group": "true",
+        "awslogs-region": "us-east-1",
+        "awslogs-stream-prefix": "ecs",
+        "mode": "non-blocking",
+        "max-buffer-size": "25m"
+      }
+    }
+  }
+]
+EOF
+)
+
+echo "Create Task Definition"
+
+ECS_TASK_DEFINITION=$(aws ecs register-task-definition \
+    --family wordpress-td \
+    --network-mode awsvpc \
+    --requires-compatibilities FARGATE \
+    --cpu "1024" \
+    --memory "3072" \
+    --execution-role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/OurEcsTaskExecutionRole \
+    --task-role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/OurEcsTaskRole \
+    --runtime-platform '{
+        "cpuArchitecture": "X86_64",
+        "operatingSystemFamily": "LINUX"
+    }'  \
+    --container-definitions "$container_definitions")
