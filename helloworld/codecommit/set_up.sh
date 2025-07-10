d28cfd1d5232e66fd14e48efdc3dddea3486d2a2
mxcheung <mxcheung@users.noreply.github.com>
2025-07-11 07:18:00 +1000

    Update set_up.sh

--- helloworld/codecommit/set_up.sh
+++ helloworld/codecommit/set_up.sh
@@ -2,3 +2,11 @@
 
 
 aws codecommit create-repository --repository-name hello-ecs
+
+cp -r $MY_ENV_DIR/aws-ecs/helloworld/hello-ecs/* .
+
+
+git add .
+git commit -m "Initial commit: Python ECS hello app"
+git push --set-upstream origin main
+
