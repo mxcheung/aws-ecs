10b6fcc6c164433db0369d2145b00e32e11f91c4
mxcheung <mxcheung@users.noreply.github.com>
2025-07-11 07:20:19 +1000

    Update set_up.sh

--- helloworld/set_up.sh
+++ helloworld/set_up.sh
@@ -13,3 +13,9 @@ cd $MY_ENV_DIR/aws-ecs/helloworld/ecr
 
 cd $MY_ENV_DIR/aws-ecs/helloworld/docker_build
 . ./set_up.sh
+
+cd $MY_ENV_DIR/aws-ecs/helloworld/ecs-task-definition
+. ./set_up.sh
+
+cd $MY_ENV_DIR/aws-ecs/helloworld/ecs
+. ./set_up.sh
