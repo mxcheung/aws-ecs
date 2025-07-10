a27798703cbe0887a626be21597ea95abc66bdfd
mxcheung <mxcheung@users.noreply.github.com>
2025-07-10 23:12:22 +1000

    Update set_up.sh

--- helloworld/ecs/set_up.sh
+++ helloworld/ecs/set_up.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Register or get the new task definition
 NEW_TASK_DEF=$(aws ecs describe-task-definition \
-  --task-definition my-task \
+  --task-definition helloword-td  \
   --query 'taskDefinition.taskDefinitionArn' \
   --output text)
+
+# Update the ECS service to use the new task definition
+aws ecs update-service \
+  --cluster my-cluster \
+  --service my-service \
+  --task-definition "$NEW_TASK_DEF"
