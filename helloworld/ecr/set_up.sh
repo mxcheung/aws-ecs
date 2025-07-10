8e5c2dd5d6efcedeb67542b92f3f17cb33acfa54
mxcheung <mxcheung@users.noreply.github.com>
2025-07-10 22:16:39 +1000

    Update set_up.sh

--- helloworld/ecr/set_up.sh
+++ helloworld/ecr/set_up.sh
@@ -1,1 +1,9 @@
-set_up.sh
+# Variables
+REPO_NAME=hello-ecs
+AWS_REGION=us-east-1
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+ECR_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}"
+
+# Create ECR repo if not exists
+aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION || true
+
