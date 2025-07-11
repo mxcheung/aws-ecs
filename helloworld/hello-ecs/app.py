dfa20a2d1748844f645e369671c02acdd27d709c
mxcheung <mxcheung@users.noreply.github.com>
2025-07-12 08:24:57 +1000

    Update app.py

--- helloworld/hello-ecs/app.py
+++ helloworld/hello-ecs/app.py
@@ -2,6 +2,15 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/actuator/health')
+def health_check():
+    return {"status": "UP"}, 200
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
+    
+
 @app.route('/wp-admin/images/wordpress-logo.svg')
 def serve_logo():
     return send_from_directory('static/wp-admin/images', 'wordpress-logo.svg')
