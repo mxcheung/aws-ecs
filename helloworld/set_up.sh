2ba983b2f4ccb69c7796cf77d572ffad0d1a9f6e
max <maxcheung@optusnet.com.au>
2025-07-12 20:16:57 +1000

    remove codebuild role in helloworld not needed

--- helloworld/set_up.sh
+++ helloworld/set_up.sh
@@ -6,10 +6,6 @@ echo $AWS_ACCESS_KEY_ID
 cd $MY_ENV_DIR/aws-ecs/helloworld/user_credentials
 . ./set_up.sh
 
-cd $MY_ENV_DIR/aws-ecs/helloworld/iam
-. ./set_up.sh
-
-
 cd $MY_ENV_DIR/aws-ecs/helloworld/codecommit
 . ./set_up.sh
 
