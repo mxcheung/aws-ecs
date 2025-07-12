fc66622fc08cb50d0fad37d092fc35b978b2273b
mxcheung <mxcheung@users.noreply.github.com>
2025-07-12 16:04:42 +1000

    Update set_up.sh

--- helloworld/iam/set_up.sh
+++ helloworld/iam/set_up.sh
@@ -2,6 +2,7 @@
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
+echo "🚀 Create role codebuild-hello-ecs-role: $AWS_ACCOUNT_ID"
 CODEBUILD_ROLE=$(aws iam create-role \
   --role-name codebuild-hello-ecs-role \
   --assume-role-policy-document file://<(cat <<EOF
@@ -18,3 +19,14 @@ CODEBUILD_ROLE=$(aws iam create-role \
 EOF
 )
 )
+
+
+echo "🚀 attach-role-policy AmazonEC2ContainerRegistryPowerUser codebuild-hello-ecs-role: $AWS_ACCOUNT_ID"
+
+CODEBUILD_ROLE_1=$(aws iam attach-role-policy --role-name codebuild-hello-ecs-role \
+  --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser)
+
+echo "🚀 attach-role-policy CloudWatchLogsFullAccess codebuild-hello-ecs-role: $AWS_ACCOUNT_ID"
+
+CODEBUILD_ROLE_2=$(aws iam attach-role-policy --role-name codebuild-hello-ecs-role \
+  --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess)
