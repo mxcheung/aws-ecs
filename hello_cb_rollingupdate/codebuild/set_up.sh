26c55ac51164a188691d98d368c6e2054f6fe4df
max <maxcheung@optusnet.com.au>
2025-07-13 15:54:17 +1000

    code build add webhook

--- hello_cb_rollingupdate/codebuild/set_up.sh
+++ hello_cb_rollingupdate/codebuild/set_up.sh
@@ -36,3 +36,10 @@ CODEBUILD_PROJECT=$(aws codebuild create-project \
   --tags key=Name,value="${PROJECT_NAME}")
 
 echo "✅ CodeBuild project '${PROJECT_NAME}' created successfully."
+
+# ──────────────── Create CodeBuild Webhook ────────────────
+
+CODEBUILD_PROJECT_WEBHOOK=$(aws codebuild create-webhook \
+  --project-name your-codebuild-project-name)
+
+echo "✅ CodeBuild project '${PROJECT_NAME}' webhook '${CODEBUILD_PROJECT_WEBHOOK}' created successfully."
