388d6f1659f66f1ca97cde2727df5c8904fafaa0
max <maxcheung@optusnet.com.au>
2025-07-15 11:34:51 +1000

    defects

--- hellocodepipeline/sqs/set_up.sh
+++ hellocodepipeline/sqs/set_up.sh
@@ -26,6 +26,8 @@ PIPELINE_NAME="hello-ecs-pipeline"
 PIPELINE_ARN="arn:aws:codepipeline:${REGION}:${AWS_ACCOUNT_ID}:${PIPELINE_NAME}"
 EVENTBRIDGE_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${EVENTBRIDGE_ROLE_NAME}"
 DLQ_ARN="arn:aws:sqs:${REGION}:${AWS_ACCOUNT_ID}:${DLQ_NAME}"
+CODE_COMMIT_RULE_NAME="CodeCommitPushTriggerRule"
+CODE_COMMIT_TRIGGER_RULE_ARN="arn:aws:events:${REGION}:${AWS_ACCOUNT_ID}:rule/${CODE_COMMIT_RULE_NAME}"
 
 echo "📬 Creating SQS DLQ: ${DLQ_NAME}"
 DLQ_URL=$(aws sqs create-queue --queue-name "${DLQ_NAME}" \
