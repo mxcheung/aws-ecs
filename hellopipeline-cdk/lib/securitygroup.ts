a741dbadc6776f45122df6fea6e79f5b3085929e
mxcheung <mxcheung@users.noreply.github.com>
2025-08-02 10:58:07 +1000

    Create securitygroup.ts

---
+++ hellopipeline-cdk/lib/securitygroup.ts
@@ -0,0 +1,43 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class MyEcsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
+
+    // Cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', { vpc });
+
+    // Security Group
+    const mySecurityGroup = new ec2.SecurityGroup(this, 'MySG', {
+      vpc,
+      description: 'Allow inbound traffic',
+      allowAllOutbound: true,
+    });
+
+    // Allow traffic on port 80
+    mySecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP');
+
+    // Task Definition
+    const taskDef = new ecs.FargateTaskDefinition(this, 'TaskDef');
+
+    taskDef.addContainer('MyContainer', {
+      image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    // ECS Service with the custom SG
+    new ecs.FargateService(this, 'MyService', {
+      cluster,
+      taskDefinition: taskDef,
+      assignPublicIp: true,
+      securityGroups: [mySecurityGroup], // 👈 ADD SECURITY GROUP HERE
+      desiredCount: 1,
+    });
+  }
+}
